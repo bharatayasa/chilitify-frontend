71af6520b554fa347cc68b8058f2bb6cd4a74469
bharatayasa <bharatayasa40@gmail.com>
2024-07-29 22:11:09 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function App() {
+	return (
+		<div className="container mx-auto">
+			<h1 className="text-3xl font-semibold text-slate-600">
+				tes
+			</h1>
+		</div>
+	)
+}
+
+export default App
